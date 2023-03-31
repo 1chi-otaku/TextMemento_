86391910a1be1d289e91155939103603518c94de
Serhii <mywaifuqueen@gmail.com>
2023-03-31 09:17:57 +0100

    Add project files.

---
+++ TextMemento_/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextMemento_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Redactor redactor = new Redactor("Hello ");
+            CareTaker taker = new CareTaker();
+
+            taker.Add(redactor.SaveMemento());
+
+            redactor.AddText("World! ");
+            taker.Add(redactor.SaveMemento());
+
+            redactor.AddText("I love blueberries! ");
+            taker.Add(redactor.SaveMemento());
+
+            redactor.AddText("Blueberries are great.");
+            taker.Add(redactor.SaveMemento());
+
+            taker.ShowHistory();
+
+            Console.WriteLine();
+            for (int i = taker.GetList().Count; i > 0;i--)
+            {
+                redactor.RestoreMemento(taker.GetList()[i-1]);
+                redactor.Print();
+                Console.WriteLine();
+            }
+           
+
+            
+
+        }
+    }
+}
---
+++ TextMemento_/Redactor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TextMemento_
+{
+    class Memento
+    {
+        string text;
+        private DateTime _date;
+
+        public Memento(string text)
+        {
+            this.text = text;
+            this._date = DateTime.Now;
+        }
+        public string GetText()
+        {
+            return text;
+        }
+        public void SetText(string text)
+        {
+            this.text = text;
+        }
+        public DateTime GetDate()
+        {
+            return this._date;
+        }
+
+    };
+    class Redactor
+    {
+        string text;
+
+        public Redactor(string text)
+        {
+            this.text = text;
+        }
+        public void AddText(string str)
+        {
+            this.text += str;
+        }
+        public Memento SaveMemento()
+        {
+            Console.WriteLine("Saving memento...");
+            return new Memento(text);
+        }
+        public void RestoreMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring memento...");
+            this.text = memento.GetText();
+        }
+        public void Print()
+        {
+            Console.Write("Redactor: " + text);
+        }
+    }
+
+    class CareTaker
+    {
+        private List<Memento> _mementos = new List<Memento>();
+
+        public void Add(Memento memento)
+        {
+            if(_mementos.Count == 256)
+            {
+                _mementos.RemoveAt(0);
+            }
+            _mementos.Add(memento);
+        }
+        public List<Memento> GetList() {
+            return _mementos;
+        }
+        public void ShowHistory()
+        {
+            Console.WriteLine("Caretaker: Here's the list of mementos:");
+
+            foreach (var memento in this._mementos)
+            {
+                Console.WriteLine(memento.GetDate() + " - " + memento.GetText());
+            }
+        }
+    }
+}
+
